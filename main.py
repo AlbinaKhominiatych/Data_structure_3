2bc7ae83c3b7264b7dcc6aa53448d5a5f422bfbb
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-04 19:35:10 +0200

    черга через бібіліотеку колекції

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-#черга через список
-queue = []
-queue.append("a")
-queue.append("b")
-queue.append("c")
-print(queue)
-print(queue.pop(0))
-#queue.append(queue.pop(0)) #Кругова черга (Ring Buffer)
-print(queue)
+#черга через бібіліотеку колекції
+from collections import deque
+q = deque()
+
+q.append("a")
+q.append("b")
+q.append("c")
+q.append("d")
+print(q)
+q.rotate(2)
+print(q)
+
