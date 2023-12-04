4c484e36fe70c45384595739be76c7f47f22977a
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-04 19:49:43 +0200

    черга через бібіліотеку queue

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#черга через бібіліотеку колекції
-from collections import deque
-q = deque()
-
-q.append("a")
-q.append("b")
-q.append("c")
-q.append("d")
-print(q)
-q.rotate(2)
-print(q)
-
+#черга через бібіліотеку queue
+from queue import Queue
+q = Queue(maxsize=3)
+print(q.qsize())
+q.put("a")
+q.put("b")
+q.put("c")
+q.get("c")
+q.put("d")
+q.put_nowait("e")
+print(q.queue)
+print(q.qsize())
